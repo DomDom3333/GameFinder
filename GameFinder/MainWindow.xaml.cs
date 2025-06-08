c8af098b1298d9d3b6627846097935103c6d5218
DomDom3333 <Dominik.essenhofer@gmail.com>
2025-06-09 01:16:14 +0200

    Improve UI with modern WPFUI controls

--- GameFinder/MainWindow.xaml.cs
+++ GameFinder/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Text.Json;
 using System.Windows;
@@ -7,14 +7,17 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Input;
 using GameFinder.Objects;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
 
 namespace GameFinder
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : FluentWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            ApplicationThemeManager.Apply(this);
         }
     }
 }
