14f6698b1a0f60bf8f382bf215b2ed1cf1138279
DomDom3333 <Dominik.essenhofer@gmail.com>
2025-11-05 13:04:58 +0100

    Add settings tab view

---
+++ ArcadeMatch.Avalonia/Controls/SettingsTabView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ArcadeMatch.Avalonia.Controls;
+
+public partial class SettingsTabView : UserControl
+{
+    public SettingsTabView()
+    {
+        InitializeComponent();
+    }
+}
